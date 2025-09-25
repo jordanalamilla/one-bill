dc26c3d09be615f565b458610baf1ca0df502af1
Jordan Michael Alamilla <jordan.alamilla@gmail.com>
2025-09-25 10:07:07 -0400

    finished basic serv.js setup with simple route, installed dotenv, updated gitignore

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Routes
+app.get('/api', (req, res) => {
+    res.send("This is the api page. Sorry I'm naked, you're pretty early.");
+});
+
+// Start the server.
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
