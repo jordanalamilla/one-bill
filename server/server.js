463f3aeccbc64e112246a3cb38c002453a4ff04b
Jordan Michael Alamilla <jordan.alamilla@gmail.com>
2025-09-25 11:11:34 -0400

    server - added status to response

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001;
 
 // Routes
 app.get('/api', (req, res) => {
-    res.send("This is the api page. Sorry I'm naked, you're pretty early.");
+    res.status(200).send("This is the api page. Sorry I'm naked, you're pretty early.");
 });
 
 // Start the server.
