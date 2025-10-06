778a134f7bce69da5f7ec2e2280b55a08163e00b
Jordan Michael Alamilla <jordan.alamilla@gmail.com>
2025-10-06 09:11:51 -0400

    connected to the database
    installed mongoose, added mongo database connection

---
+++ server/src/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const DB_URI = process.env.DB_URI;
+
+// Connect to the database with Mongoose.
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(DB_URI);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error.message);
+        process.exit(1);
+    }
+}
--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
 import billRoutes from './routes/billRoutes.js';
+import { connectDB } from './config/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Routes
+// Connect to the database.
+connectDB();
+
+// Use the routes.
 app.use('/api/bills', billRoutes);
 
 // Start the server.
