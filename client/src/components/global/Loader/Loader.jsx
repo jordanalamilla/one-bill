70580810cb4250b3728cf5f65df72f83e21ae691
Jordan Michael Alamilla <jordan.alamilla@gmail.com>
2025-11-05 15:33:01 -0500

    loader - added basic styles

--- client/src/components/global/Loader/Loader.jsx
+++ client/src/components/global/Loader/Loader.jsx
@@ -1,6 +1,19 @@
+import './Loader.scss'
+
 const Loader = () => {
     return (
-        <div>Loading</div>
+        <section className="loader-section">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+
+                        <div className='loader-text'>Loading</div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
     )
 }
 
