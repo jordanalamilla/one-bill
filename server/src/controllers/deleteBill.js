f4aac4ba09a1820c978e26311a6a5aec8e472a7a
Jordan Michael Alamilla <jordan.alamilla@gmail.com>
2025-11-24 20:19:11 -0500

    controllers - delete - updated logic to handle bill not found

--- server/src/controllers/deleteBill.js
+++ server/src/controllers/deleteBill.js
@@ -7,15 +7,21 @@
 import Bill from "../models/Bill.js";
 
 export async function deleteBill(req, res) {
-    try {
-        // Find the Bill by ID and delete it.
-        await Bill.findByIdAndDelete(req.params.id).then(deletedBill => {
-            res.status(200).json({ deletedBill });
-        });
+    // Find the Bill by ID and delete it.
+    await Bill.deleteOne({ _id: req.params.id }).then((deletedBill) => {
+        if (!deletedBill.deletedCount) {
+            res.status(404).json({
+                error: 'Bill not found.'
+            });
 
-    } catch (error) {
-        // Error handling.
-        res.status(404).json({ message: "Bill not found." });
-        console.error("Error caught in updateBill() controller: ", error);
-    }
+        } else {
+            res.status(200).send({
+                message: "Bill deleted",
+            });
+        }
+    }).catch(error => {
+        res.status(500).json({
+            error: error.message
+        });
+    });
 }
